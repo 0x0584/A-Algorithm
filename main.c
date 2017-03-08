4490ecbd80a2c94542d3c64ece9580bec13851f3
Anas Rchid <rchid.anas@gmail.com>
2017-03-08 15:23:33 +0000

    ADD: print the amze using ansi escape-characters

--- main.c
+++ main.c
@@ -1,10 +1,40 @@
+/* note to self:
+ *
+ * you vs time; who would be the winner? 
+ * trust me, you don't really what would happen 
+ * if you lose; an undefined behavior!
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
+/* foreground colors */
+#define FBLACK	 30
+#define FWHITE   37
+
+#define FRED	 31
+#define FGREEN	 32
+#define FYELLOW  33
+#define FBLUE	 34
+#define FMAGENTA 35
+#define FCYAN	 36
+
+/* background colors */
+#define BBLACK	 40
+#define BWHITE   47
+
+#define BRED	 41
+#define BGREEN	 42
+#define BYELLOW  43
+#define BBLUE	 44
+#define BMAGENTA 45
+#define BCYAN	 46
+
+
 /* maze dimensions */
-#define XDIM 8
-#define YDIM XDIM
+#define XDIM 30
+#define YDIM (2 * XDIM)
 
 #define START 0
 #define TARGET 1
@@ -160,16 +190,36 @@ putmaze(maze_t *m)
       uint xstart = m->area[START].x, ystart = m->area[START].y,
 	xtarget = m->area[TARGET].x, ytarget = m->area[TARGET].y;
       char nodeval;	/* hold the node value */
-
+#define STARTCHAR 'S'
+#define TARGETCHAR 'T'
+#define WALLCHAR '#'
+#define ROADCHAR '.'
+      
       /* if the node is the starting/target node */
-      if(i == xstart &&  j == ystart) nodeval = 'S';
-      else if (i == xtarget && j == ytarget) nodeval = 'T';
+      if(i == xstart &&  j == ystart) nodeval = STARTCHAR;
+      else if (i == xtarget && j == ytarget) nodeval = TARGETCHAR;
       /* determinate the `nodeval` based on its type */
-      else if(wall) nodeval = '+';
-      else nodeval = '.';
-
-      /* put it on the screen */
-      putchar(nodeval);
+      else if(wall) nodeval = WALLCHAR;
+      else nodeval = ROADCHAR;
+      /*
+      for(int i = 20; i < 50; ++i) {
+	printf("i:%d\n", i);
+	for(j = 20; j < 50; ++j)
+	  printf("\033[%d;%dmj:\t%d \033[0;37m ",i, j,j);
+	//      getchar();
+        printf("\n\n\n");
+      }
+      */
+#define putcolored(C, BACK, FORE) \
+      printf("\033[%d;%dm%c\033[%d;%dm", FORE, BACK,	\
+	     C, FWHITE, BBLACK)
+      
+      switch(nodeval) {
+      case WALLCHAR: putcolored(nodeval, FBLACK, BBLUE); break;
+      case STARTCHAR: putcolored(nodeval, FBLACK, BGREEN); break;
+      case TARGETCHAR: putcolored(nodeval, FBLACK, BRED); break;
+      default: putcolored(nodeval, FBLACK, BBLACK); break;
+      };
     }    
     putchar('\n');
   }
@@ -187,36 +237,17 @@ seek(maze_t *m, cord_t *a)
 #define UPLEFT(w, x, y) w[x - 1][y - 1]
 #define DOWNLEFT(w, x, y) w[x + 1][y - 1]
 #define UPRIGHT(w, x, y) w[x - 1][y + 1]
-#define DOW , *closeNRIGHT(w, x, y) w[x + 1]
-  /* the list structure, which would hold an 
-   * array of node-coordinatess */
-  struct LIST {
-    uint size;			    /* elements count */
-    node_t **list;		    /* array of node-coordinates */
-  } *open;		    /* our open/close list */
-
+#define DOWNRIGHT(w, x, y) w[x + 1]
 
   /* local functions */  
-  struct LIST *listinit(uint __size) {
-    index i;
-    struct LIST *foolist;
-
-    foolist = (struct LIST *) calloc(1, sizeof(struct LIST));
-    foolist->list = (node_t **) calloc(__size, sizeof(node_t *));
-    foolist->size = __size;
-
-    for(i = 0; i < __size; ++i) foolist->list[i] = NULL;
-    
-    return foolist;
-  };
   bool iswalkable(node_t *__node, cord_t *__parent) {
     return !(__node->iswall) ? ( __node->parent = __parent), false : true;
   };
   cord_t nextstep(node_t **__map,
-		  const cord_t __node,
-		  struct LIST *__olist, struct LIST *__clist) {
+		  const cord_t __node) {
     cord_t foocord;
 
+    
     
     return foocord;
   };
@@ -226,15 +257,21 @@ seek(maze_t *m, cord_t *a)
     sizeofmaze;			    /* # of nodes in the maze */
   uint xtarget = a[TARGET].x,
     ytarget = a[TARGET].y;
-
+  cord_t **path;
   bool isfirsttime = true;	    /* this one is to tell whether:
 				     * (we are at the starting area) ||
 				     * (on our way through the maze)
 				     */
 
-  /* body */
+  /* function body */
   sizeofmaze = m->xdim * m->ydim;   /* number of nodes */
-  
+
+  /* allocate memory for the map */
+  path = (cord_t **) malloc(sizeofmaze * sizeof(cord_t *));
+
+  for(i = 0; i < sizeofmaze; ++i)
+    path[i] = NULL;
+
   /* TODO:
    * 
    * this is a temporary version of this
@@ -242,12 +279,17 @@ seek(maze_t *m, cord_t *a)
    * the problem with realloc()!
    */
 
-  /* allocate memory, as always.. */
-  open = listinit(sizeofmaze);
+  bool isthereopen(maze_t *m) {
+    index i;
+    
+    for(i = 0; i < m->xdim; ++i)
+      for(j = 0; j < m->ydim; ++j)
+	if(m->map[i][j].isopen == true) return true;
 
-  
+    return false;
+  };
   /* seeking the target.. */
-  while((open->size || isfirsttime) {
+  while(isthereopen(m) ^ isfirsttime) {
     cord_t current;		   /* the current node */
 
     if(isfirsttime) {
@@ -257,36 +299,14 @@ seek(maze_t *m, cord_t *a)
       current = a[START];
     }
     else
-      current = nextstep(m->map, current, open, close);
+      current = nextstep(m->map, current);
     
     printf("\nocount:\t%d\t\tccount:\t%d\ncurrent(%d, %d)\n",
-	   open->size, close->size,
 	   current.x, current.y);
-    
-    /*
-    for(i = 0; i < m->xdim; ++i) {
-      for(j = 0; j < m->ydim; ++j) {
-
-	char state[] = { 'w', 'n' };
-	bool s = iswalkable(&(m->map[i][j]), &a[START]);
-
-	printf("%c", state[s]);
-	
-	if(m->map[i][j].parent != NULL)
-	  printf(" %d, %d", m->map[i][j].parent->x,
-		 m->map[i][j].parent->y);
-	
-	putchar('\t');
-      }
-      putchar('\n');
-    }
-    */
     break;
   }
 
   /* always free you memory.. */
-  free(open->list), free(close->list);
-  free(close), free(open);
 }
 
 void
